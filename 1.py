57532470acbd868cbbb2c9d4acb79f25775f8c61
Ben Pinchas <benphs5@gmail.com>
2021-09-12 23:12:04 +0300

    added assignment 1

---
+++ 1.py
@@ -0,0 +1,30 @@
+first = 7
+
+second = 44.3
+
+# A 3
+print(first + second)
+
+# A 4
+print(first * second)
+
+# A 5
+print(second / first)
+
+# B
+# Answer: c = 15, a = 9, b = 8
+
+# C
+# Answer: there is no differece
+
+# Answer: we can't use + on values of types 'str' and 'int'
+
+# D
+x = 5
+y = 2.36
+print(x + int(y)) # -> 7
+
+# CHALLENGE
+a = 8
+b = "123"
+print(a + int(b))
